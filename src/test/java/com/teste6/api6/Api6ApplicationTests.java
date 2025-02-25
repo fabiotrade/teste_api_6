03705994688c7679adaa1864aa46562aedce2ee9
fabiotrade <fabiomaimone@gmail.com>
2025-02-25 20:23:15 -0300

    primeiro commit teste

---
+++ src/test/java/com/teste6/api6/Api6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teste6.api6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Api6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
